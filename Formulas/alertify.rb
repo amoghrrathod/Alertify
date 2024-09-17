b919e31b1fead274d1fb231488822f3d295fabca
amoghrathod <amoghrrathod@gmail.com>
2024-09-17 12:12:56 +0530

    Release version 1.0.1

--- Formulas/alertify.rb
+++ Formulas/alertify.rb
@@ -4,9 +4,6 @@ class Alertify < Formula
   url "https://github.com/amoghrrathod/alertify/releases/download/v1.0.1/alertify-1.0.1.tar.gz"
   sha256 "e00f1ec48ef7cc3f5237f3669ec5c505d8814d529e873e7038bad05fcbce2812"
 
-  depends_on "jansson"
-  depends_on "uuid"
-
   def install
     system "make", "PREFIX=#{prefix}"
   end
