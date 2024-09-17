262cd3fab0aa0d8cb1ba4e34712b28bb0d42cdac
amoghrathod <amoghrrathod@gmail.com>
2024-09-17 12:09:19 +0530

    Release version 1.0.1

--- Formulas/alertify.rb
+++ Formulas/alertify.rb
@@ -1,7 +1,7 @@
 class Alertify < Formula
   desc "CLI tool for managing reminders"
   homepage "https://github.com/amoghrrathod/alertify"
-  url "https://github.com/amoghrrathod/alertify/releases/download/v1.0.0/alertify-1.0.1.tar.gz"
+  url "https://github.com/amoghrrathod/alertify/releases/download/v1.0.1/alertify-1.0.1.tar.gz"
   sha256 "e00f1ec48ef7cc3f5237f3669ec5c505d8814d529e873e7038bad05fcbce2812"
 
   depends_on "jansson"
